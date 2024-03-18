22740a7d8cb6c476fd52ca6a666278eedc611aae
Rayhub <1102290321@qq.com>
2024-03-18 10:46:58 +0000

    fix script

--- 01-efficiency/run.sh
+++ 01-efficiency/run.sh
@@ -15,13 +15,21 @@ fi
 
 REPEAT=5
 targets=(STM32F072 STM32F103 STM32F407 STM32F769 STM32L073 NXP-FRDM-K22F NXP-FRDM-K64F NXP-FRDM-K82F NXP-FRDM-KL25Z NXP-LPCXpresso51U68 SAM4E-Xplained-Pro SAM4L-EK SAM4S-Xplained SAME70-Xplained SAMV71-Xplained-Ultra SAM3X8E)
+
+for i in $(seq 1 $REPEAT); do
+    if [ ! -d "$DIR/exp-$i" ]; then
+        mkdir "$DIR/exp-$i"
+    fi
+done
+
 for target in "${targets[@]}"
 do
     for i in $(seq 1 $REPEAT); do
         echo "[+] running Perry on $target, round $i"
         begin_time=$(date +%s%N)
-        "$DIR"/run_one.sh "$PERRY_PATH"/synthesizer/example/"$target" "$DIR" &> "$DIR"/"$target".log
+        python "$PERRY_PATH"/synthesizer/synthesize.py -c "$PERRY_PATH/synthesizer/example/$target/config.yaml" -o "$DIR/exp-$i" -a &> "$DIR/exp-$i/$target.log"
         end_time=$(date +%s%N)
+        mv perry-*.json "$DIR/exp-$i"
         elapsed_time=$(python -c "print(($end_time - $begin_time) / 1000000000)")
         echo "$target, $i, $elapsed_time" >> "$DIR"/result.csv
     done
